13873fb5fb48f7a936d1703bb2e02ebcfa6ca65c
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2024-04-09 11:45:52 +0200

    Create main for TOSEM experiment 2

--- src/main/java/es/us/isa/jsonmutator/experiment2/MainExperiment2.java
+++ src/main/java/es/us/isa/jsonmutator/experiment2/MainExperiment2.java
@@ -1,105 +1,52 @@
 package es.us.isa.jsonmutator.experiment2;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
-import static es.us.isa.jsonmutator.experiment2.GenerateAssertions.getOutputPath;
 import static es.us.isa.jsonmutator.experiment2.MutateTestCases.mutateTestCases;
 
+/**
+ * @author Juan C. Alonso
+ */
 public class MainExperiment2 {
 
-    private static int nExecutions = 100;
-    private static String mutatedTestCasesFolder = "mutated_test_cases";
-    private static String mutationReportFolder = "mutation_reports";
+    // Number of mutated versions of the original test suite to generate
+    private static int nExecutions = 2;
 
-    private static boolean generateNewMutants = true;
+    // Location of the CSV file containing the original test cases
+    private static String originalTestCasesPath;
 
-    public static void main(String[] args) throws Exception {
+    // Directory that will store the resulting mutated test suites (one per execution)
+    private static String mutatedTestCasesDirectory;
 
-        // Read invariants and test cases paths from JSON file
-        ObjectMapper objectMapper = new ObjectMapper();
-        ArrayNode operationPathList = null;
-        try {
-            operationPathList = objectMapper.readValue(new String(Files.readAllBytes(Paths.get("src/main/resources/experiment2/operationsPaths.json"))), ArrayNode.class);
-        } catch (IOException e) {
-            e.printStackTrace();
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 3) {
+           nExecutions = Integer.parseInt(args[0]);
+           originalTestCasesPath = args[1];
+
+           mutatedTestCasesDirectory = args[2];
         }
 
-        // For every API operation
-        for(JsonNode operationPath: operationPathList) {
+        // Delete previous mutants (if any) and create an empty one
+        File mutatedTestCasesFile = new File(mutatedTestCasesDirectory);
+        FileUtils.deleteDirectory(mutatedTestCasesFile);
+        mutatedTestCasesFile.mkdir();
 
-            String operationName = operationPath.get("operationName").textValue();
-            String testCasesPath = operationPath.get("testCasesPath").textValue();
-            String invariantsPath = operationPath.get("invariantsPath").textValue();
+        // Generate mutant files, a total of nExecutions times
+        for (int i = 1; i <= nExecutions; i++) {
 
-            System.out.println(operationName);
+            Path mutatedTestCasesPath = Paths.get(mutatedTestCasesDirectory, String.format("mutants_%04d.csv", i));
 
-            List<String> stringsToConsiderAsNull = new ArrayList<>();
-            JsonNode jsonNullStrings = operationPath.get("stringsToConsiderAsNull");
-            if(jsonNullStrings.isArray()) {
-              for(JsonNode node: jsonNullStrings) {
-                  stringsToConsiderAsNull.add(node.textValue());
-              }
-            }
-
-            // Assert that no mutants are killed in the original file (without mutate)
-            GenerateAssertions originalAssertions = new GenerateAssertions(testCasesPath, invariantsPath, stringsToConsiderAsNull);
-            Double percentageKilledOriginal = originalAssertions.generateAssertions(operationName + "_original_mutation_report.csv");
-            if(percentageKilledOriginal != 0.0) {
-                throw new Exception("The mutation score in the original test suite should be 0%");
-            }
-
-            // Remove directories if exist and create empty directory
-            if(generateNewMutants) {    // Delete previous mutants
-                File mutatedTestCasesFile = new File(getOutputPath(mutatedTestCasesFolder, testCasesPath));
-                FileUtils.deleteDirectory(mutatedTestCasesFile);
-                mutatedTestCasesFile.mkdir();
-            }
-            File mutationReportFile = new File(getOutputPath(mutationReportFolder, testCasesPath));
-            FileUtils.deleteDirectory(mutationReportFile);
-            mutationReportFile.mkdir();
-
-            List<Double> mutationScores = new ArrayList<>();
-            // Generate and kill mutants, total of nExecutions
-            for(int i = 0; i<nExecutions; i++) {
-                String mutatedTestCasesPath = getOutputPath(mutatedTestCasesFolder + "/" + operationName + "_mutants_" + i + ".csv", testCasesPath);
-                if(generateNewMutants) {
-                    // Generate mutated_testCases.csv, returns the path of the mutants file
-                    mutatedTestCasesPath = mutateTestCases(mutatedTestCasesFolder + "/" + operationName + "_mutants_" + i + ".csv", testCasesPath);
-                }
-
-                // Generate mutation reports
-                GenerateAssertions generateAssertions = new GenerateAssertions(mutatedTestCasesPath, invariantsPath, stringsToConsiderAsNull);
-                try {
-                    // Returns the percentage of mutants killed and generates a report in csv format
-                    Double percentageKilled = generateAssertions.generateAssertions(mutationReportFolder + "/" + operationName + "_mutation_report_" + i + ".csv");
-                    mutationScores.add(percentageKilled);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-            // Mutation scores for 10 executions
-//            System.out.println(mutationScores);
-            Double averageMutationScore = mutationScores.stream().mapToDouble(val ->val).average().orElse(0.0);
-            System.out.println(mutationScores);
-            System.out.println(averageMutationScore);
-
-
-
+            // Generate mutated test cases
+            mutateTestCases(mutatedTestCasesPath.toString(), originalTestCasesPath);
 
         }
-
-
 
     }
 }
---
+++ src/main/java/es/us/isa/jsonmutator/experiment2/MainExperiment2_old.java
@@ -0,0 +1,105 @@
+package es.us.isa.jsonmutator.experiment2;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static es.us.isa.jsonmutator.experiment2.GenerateAssertions.getOutputPath;
+import static es.us.isa.jsonmutator.experiment2.MutateTestCases.mutateTestCases;
+
+public class MainExperiment2_old {
+
+    private static int nExecutions = 100;
+    private static String mutatedTestCasesFolder = "mutated_test_cases";
+    private static String mutationReportFolder = "mutation_reports";
+
+    private static boolean generateNewMutants = true;
+
+    public static void main(String[] args) throws Exception {
+
+        // Read invariants and test cases paths from JSON file
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayNode operationPathList = null;
+        try {
+            operationPathList = objectMapper.readValue(new String(Files.readAllBytes(Paths.get("src/main/resources/experiment2/operationsPaths.json"))), ArrayNode.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // For every API operation
+        for(JsonNode operationPath: operationPathList) {
+
+            String operationName = operationPath.get("operationName").textValue();
+            String testCasesPath = operationPath.get("testCasesPath").textValue();
+            String invariantsPath = operationPath.get("invariantsPath").textValue();
+
+            System.out.println(operationName);
+
+            List<String> stringsToConsiderAsNull = new ArrayList<>();
+            JsonNode jsonNullStrings = operationPath.get("stringsToConsiderAsNull");
+            if(jsonNullStrings.isArray()) {
+              for(JsonNode node: jsonNullStrings) {
+                  stringsToConsiderAsNull.add(node.textValue());
+              }
+            }
+
+            // Assert that no mutants are killed in the original file (without mutate)
+            GenerateAssertions originalAssertions = new GenerateAssertions(testCasesPath, invariantsPath, stringsToConsiderAsNull);
+            Double percentageKilledOriginal = originalAssertions.generateAssertions(operationName + "_original_mutation_report.csv");
+            if(percentageKilledOriginal != 0.0) {
+                throw new Exception("The mutation score in the original test suite should be 0%");
+            }
+
+            // Remove directories if exist and create empty directory
+            if(generateNewMutants) {    // Delete previous mutants
+                File mutatedTestCasesFile = new File(getOutputPath(mutatedTestCasesFolder, testCasesPath));
+                FileUtils.deleteDirectory(mutatedTestCasesFile);
+                mutatedTestCasesFile.mkdir();
+            }
+            File mutationReportFile = new File(getOutputPath(mutationReportFolder, testCasesPath));
+            FileUtils.deleteDirectory(mutationReportFile);
+            mutationReportFile.mkdir();
+
+            List<Double> mutationScores = new ArrayList<>();
+            // Generate and kill mutants, total of nExecutions
+            for(int i = 0; i<nExecutions; i++) {
+                String mutatedTestCasesPath = getOutputPath(mutatedTestCasesFolder + "/" + operationName + "_mutants_" + i + ".csv", testCasesPath);
+                if(generateNewMutants) {
+                    // Generate mutated_testCases.csv, returns the path of the mutants file
+                    mutatedTestCasesPath = mutateTestCases(mutatedTestCasesFolder + "/" + operationName + "_mutants_" + i + ".csv", testCasesPath);
+                }
+
+                // Generate mutation reports
+                GenerateAssertions generateAssertions = new GenerateAssertions(mutatedTestCasesPath, invariantsPath, stringsToConsiderAsNull);
+                try {
+                    // Returns the percentage of mutants killed and generates a report in csv format
+                    Double percentageKilled = generateAssertions.generateAssertions(mutationReportFolder + "/" + operationName + "_mutation_report_" + i + ".csv");
+                    mutationScores.add(percentageKilled);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+            // Mutation scores for 10 executions
+//            System.out.println(mutationScores);
+            Double averageMutationScore = mutationScores.stream().mapToDouble(val ->val).average().orElse(0.0);
+            System.out.println(mutationScores);
+            System.out.println(averageMutationScore);
+
+
+
+
+        }
+
+
+
+    }
+}
